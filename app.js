6bf1115c03603bd0564eb4d715bee0e725e3027d
Ali Morshid <noneisnull@gmail.com>
2018-06-06 19:57:04 -0400

    Fixed issue with layout not resizing.

--- app.js
+++ app.js
@@ -9,4 +9,4 @@ app.all('/', function (req, res) {
 
 app.use(express.static("public"));
 
-app.listen(process.env.PORT || 5000, () => console.log('App listening on port 5000!'));
+app.listen(process.env.PORT || 1234, () => console.log('App listening on port 1234!'));
