50770ced77fd2c1cdef423c711e537ca9588e4d2
Gary Muffoletto II <garymuff@buffalo.edu>
2018-06-15 23:18:22 -0400

    Javascript functionality added to the Backspace button.

--- public/js/main.js
+++ public/js/main.js
@@ -1,68 +1,105 @@
 window.onload = function(){ 
-	// Begin code for button click
+
+
+
+	//run the backspace display function to show/hide backspace
+	bsdiplay();
+	//Backspace button functionality
+	document.getElementById("backspace").onclick = function() {
+		var bs = document.getElementById("numberpaddisplay");
+		if (isEmpty(bs) == false){
+			bs.value = bs.value.substring(0, bs.value.length - 1);
+		}
+		//run the backspace display function to show/hide backspace
+		bsdiplay();
+	};
+
+	// Begin code for numpad button click
 	document.getElementById("zero").onclick = function() {
 		var add = document.getElementById("numberpaddisplay");
 		if (ismaxlength(add) == false){
 			add.value += '0';
 		}
+		//run the backspace display function to show/hide backspace
+		bsdiplay();
 	};
 	document.getElementById("one").onclick = function() {
 		var add = document.getElementById("numberpaddisplay");
 		if (ismaxlength(add) == false){
 			add.value += '1';
 		}
+		//run the backspace display function to show/hide backspace
+		bsdiplay();
 	};
 	document.getElementById("two").onclick = function() {
 		var add = document.getElementById("numberpaddisplay");
 		if (ismaxlength(add) == false){
 			add.value += '2';
 		}
+		//run the backspace display function to show/hide backspace
+		bsdiplay();
 	};
 	document.getElementById("three").onclick = function() {
 		var add = document.getElementById("numberpaddisplay");
 		if (ismaxlength(add) == false){
 			add.value += '3';
 		}
+		//run the backspace display function to show/hide backspace
+		bsdiplay();
 	};
 	document.getElementById("four").onclick = function() {
 		var add = document.getElementById("numberpaddisplay");
 		if (ismaxlength(add) == false){
 			add.value += '4';
 		}
+		//run the backspace display function to show/hide backspace
+		bsdiplay();
 	};
 	document.getElementById("five").onclick = function() {
 		var add = document.getElementById("numberpaddisplay");
 		if (ismaxlength(add) == false){
 			add.value += '5';
 		}
+		//run the backspace display function to show/hide backspace
+		bsdiplay();
 	};
 	document.getElementById("six").onclick = function() {
 		var add = document.getElementById("numberpaddisplay");
 		if (ismaxlength(add) == false){
 			add.value += '6';
 		}
+		//run the backspace display function to show/hide backspace
+		bsdiplay();
 	};
 	document.getElementById("seven").onclick = function() {
 		var add = document.getElementById("numberpaddisplay");
 		if (ismaxlength(add) == false){
 			add.value += '7';
 		}
+		//run the backspace display function to show/hide backspace
+		bsdiplay();
 	};
 	document.getElementById("eight").onclick = function() {
 		var add = document.getElementById("numberpaddisplay");
 		if (ismaxlength(add) == false){
 			add.value += '8';
 		}
+		//run the backspace display function to show/hide backspace
+		bsdiplay();
 	};
 	document.getElementById("nine").onclick = function() {
 		var add = document.getElementById("numberpaddisplay");
 		if (ismaxlength(add) == false){
 			add.value += '9';
 		}
+		//run the backspace display function to show/hide backspace
+		bsdiplay();
 	};
 	document.getElementById("del").onclick = function() {
 		var del = document.getElementById("numberpaddisplay");
 		del.value = "";
+		//run the backspace display function to show/hide backspace
+		bsdiplay();
 	};
 	document.getElementById("enter").onclick = function() {
 		var submit = document.getElementById("numberpaddisplay");
@@ -70,15 +107,39 @@ window.onload = function(){
 			document.getElementById("ledger").innerHTML += "<div class=\"item\"><div class=\"SKU sbs\">"+submit.value+"</div><div class=\"qty sbs\">1</div><div class=\"name sbs\">test</div><div class=\"price\">$0.00&nbsp;</div></div>";
 		}
 		submit.value = "";
+		//run the backspace display function to show/hide backspace
+		bsdiplay();
 	};
-	//function to check for max length of sixe digits
-	function ismaxlength(input) {
-		input = input.value
-		if (input.length < 6){
-			return false;
-		} else{
-			return true;
-		}
-	};
+	//End code for numpad button click
+
+	
 };
 
+//function to check for max length of sixe digits
+function ismaxlength(input) {
+	input = input.value
+	if (input.length < 6){
+		return false;
+	} else{
+		return true;
+	}
+};
+
+//function to check if input is empty
+function isEmpty(input) {
+	input = input.value
+	if (input.length != 0){
+		return false;
+	} else{
+		return true;
+	}
+};
+
+function bsdiplay() {
+	input = document.getElementById("numberpaddisplay").value
+	if (input.length == 0){
+		document.getElementById("backspace").style.display = "none";
+	} else{
+		document.getElementById("backspace").style.display = "inline";
+	}
+};
