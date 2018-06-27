3c27671909df690855656951d8c30901171bbd69
Ali Morshid <noneisnull@gmail.com>
2018-06-27 18:44:34 -0400

    Removed sensitive files to private directory.

--- private/routes/inventory.js
+++ private/routes/inventory.js
@@ -3,6 +3,6 @@ const path = require('path');
 const check = require('connect-ensure-login').ensureLoggedIn();
 const router = express.Router();
 
-router.get('/', check, (req, res) => {res.sendFile(path.join(__dirname, '../../public/inventory.html'))});
+router.get('/', check, (req, res) => {res.sendFile(path.join(__dirname, '../../private/inventory.html'))});
 
 module.exports = router;
